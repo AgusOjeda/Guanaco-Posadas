48e64ccc56ebcef8eb64f1be971218773b95258a
Agustin Ojeda <agustinojedadev@gmail.com>
2022-02-03 19:52:16 -0300

    factorizando el codigo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,40 +1,21 @@
 import React from "react";
 import {
-  ChakraProvider,
-  Container,
-  VStack,
-  Image,
-  Heading,
-  Text,
-  Box,
-  Divider,
+	ChakraProvider,
+	Container,
+	Box,
 } from "@chakra-ui/react";
 import { AppProps } from "next/app";
 import theme from "../theme";
 
+
+
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return (
-    <ChakraProvider theme={theme}>
-      <Box padding={4}>
-        <Container
-          borderRadius="sm"
-          backgroundColor="white"
-          boxShadow="md"
-          marginY={4}
-          maxWidth="container.xl"
-          padding={4}
-        >
-          <VStack marginBottom={6}>
-            <Image maxWidth={128} borderRadius={20} src="https://res.cloudinary.com/dywphbg73/image/upload/v1643173989/guanacoposadas/logo_yogfat.jpg"></Image>
-            <Heading>Guanaco Posadas</Heading>
-            <Text>- Accesorios bazar y regalos -</Text>
-          </VStack>
-          <Divider marginY={6}></Divider>
-          <Component {...pageProps} />
-        </Container>
-      </Box>
-    </ChakraProvider>
-  );
+	return (
+		<ChakraProvider theme={theme}>
+			
+			<Component {...pageProps} />
+		</ChakraProvider>
+	);
 };
 
 export default App;
